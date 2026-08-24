7d9ca64f73a2c47834569fed0b5925a161250b0b
AnhThy3447 <anhthyvongoc2105@gmail.com>
2026-08-24 17:13:01 +0700

    add Reset button

--- app.py
+++ app.py
@@ -10,62 +10,7 @@ current_guess = 0
 end_game = False
 
 
-# --- GAME INTERFACE ---
-root = Tk()
-root.title("Wordle")
-root.config(bg=bg_color)
-root.geometry("450x620")
-
-# Title
-Label(
-    root,
-    text="WORDLE", 
-    font=("Helvetica", 40, "bold"),
-    fg="yellow",
-    bg=bg_color
-).pack(pady=(35, 5))
-
-# Answer box
-fr_ans = Frame(root, pady=5, bg=bg_color)
-fr_ans.pack(anchor='center', expand=True)
-
-lbl = Label(fr_ans, text="Your Answer", width=10, anchor='w',
-            font=("Helvetica", 14, "bold"), bg=bg_color, fg="white")
-lbl.pack(side=LEFT, padx=5)           
-
-entry = Entry(fr_ans)
-entry.pack(side=LEFT, padx=5)
-entry.focus()
-
-# Answer
-body = Frame(root, bg=bg_color)
-body.pack(fill=BOTH, expand=True)
-
-grid = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
-
-mfield = []
-for row in range (6):
-    blocksContainer = Frame(body, bg=bg_color)
-    blocksContainer.pack()
-    for col in range(5):
-        txt = '' if grid[row][col] == 0 else grid[row][col]
-        mfield.append(Button(blocksContainer, relief="groove", bg=bg_color,highlightbackground="white", highlightcolor="white",
-                     height=1, width=4, font=("Helvetica", 20, "bold")))
-        mfield[row * 5 + col].grid(row=0, column=col, padx=2, pady=5, sticky="ew")
-
-# Correct Answer
-lab_ans = Label(root, bg=bg_color)
-lab_ans.pack(pady=(0, 10))
-
-
-# --- RANDOM WORD ---
+# --- LOAD WORDS ---
 data = pd.read_csv("data/wordle.csv")
 word_list = data['word']
 
@@ -85,12 +30,12 @@ def check_answer(user_ans, row):
     return False
 
 def handle_user_answer(event=None):
-    global current_guess, end_game
+    global current_guess, end_game, correct_ans
 
     if end_game:
         return
 
-    user_ans = entry.get()
+    user_ans = entry.get().lower()
     if len(user_ans) == 5:
         if user_ans in set(word_list):
             if check_answer(user_ans, current_guess):
@@ -110,12 +55,81 @@ def handle_user_answer(event=None):
             messagebox.showwarning("Warning", "It's not in word list")
     else:
         messagebox.showwarning("Warning", "It's a 5-letter word")
-    
+
+def new_game():
+    global wordle, correct_ans, mfield, current_guess, end_game
+    # reload UI
+    lab_ans.config(text="")
+    for row in range (6):
+        for col in range(5):
+            mfield[row * 5 + col].config(text="", relief="groove", bg=bg_color,highlightbackground="white", 
+                                         highlightcolor="white", height=1, width=4, font=("Helvetica", 20, "bold"))
+    current_guess = 0
+    end_game = False
+    entry.config(state="normal")
+    entry.delete(0, 'end')
+
+    # choose new word
+    correct_ans = str(random.choice(word_list))
+    wordle = Wordle(correct_ans)
+    root.bind('<Return>', handle_user_answer)
+
+
+# --- GAME INTERFACE ---
+root = Tk()
+root.title("Wordle")
+root.config(bg=bg_color)
+root.geometry("450x620")
+
+# Reset button
+Button(
+    root,
+    text="Reset",
+    font=("Arial", 8),
+    bg="white",
+    width=10,
+    command=new_game
+).pack(side=TOP, anchor=NE, padx=8, pady=5)
+
+# Title
+Label(
+    root,
+    text="WORDLE", 
+    font=("Helvetica", 40, "bold"),
+    fg="yellow",
+    bg=bg_color
+).pack(pady=(20, 5))
+
+# Answer box
+fr_ans = Frame(root, pady=5, bg=bg_color)
+fr_ans.pack(anchor='center', expand=True)
+
+lbl = Label(fr_ans, text="Your Answer", width=10, anchor='w',
+            font=("Helvetica", 14, "bold"), bg=bg_color, fg="white")
+lbl.pack(side=LEFT, padx=5)           
+
+entry = Entry(fr_ans)
+entry.pack(side=LEFT, padx=5)
+entry.focus()
+
+# Answer
+body = Frame(root, bg=bg_color)
+body.pack(fill=BOTH, expand=True)
+
+mfield = []
+for row in range (6):
+    blocksContainer = Frame(body, bg=bg_color)
+    blocksContainer.pack()
+    for col in range(5):
+        mfield.append(Button(blocksContainer, relief="groove", bg=bg_color,highlightbackground="white", highlightcolor="white",
+                     height=1, width=4, font=("Helvetica", 20, "bold")))
+        mfield[row * 5 + col].grid(row=0, column=col, padx=2, pady=5, sticky="ew")
+
+# Correct Answer
+lab_ans = Label(root, bg=bg_color)
+lab_ans.pack(pady=(0, 10))
+
 
 # --- MAIN ---
-correct_ans = str(random.choice(word_list))
-wordle = Wordle(correct_ans)
-print(correct_ans)
-
-root.bind('<Return>', handle_user_answer)
+new_game()
 root.mainloop()
